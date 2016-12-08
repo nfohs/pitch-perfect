3e3d95053594588797db0ace8f457eb573bd2ea7
nfohs <nick.fohs@gmail.com>
2016-12-07 16:34:19 -0800

    added basic buttons and layout

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var recordingLabel: UILabel!
+  
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+  @IBAction func recordAudio(_ sender: Any) {
+    print("Record button was pressed")
+    recordingLabel.text = "Recording in Progress"
+  }
 
+  @IBAction func stopRecording(_ sender: Any) {
+    print("Stop Recording was pressed") 
+  }
 }
 
